17abf060cfe84a6294f07498752feb95a5e4012a
bqtruong2000 <bqtruong2021@gmail.com>
2024-11-19 21:21:00 +0700

    #12-Validation with custom annotation

--- identity-service/src/main/java/core/identityservice/dto/request/UserUpdateRequest.java
+++ identity-service/src/main/java/core/identityservice/dto/request/UserUpdateRequest.java
@@ -17,6 +17,7 @@ public class UserUpdateRequest {
     String firstName;
     String lastName;
     @DobConstraint(min = 18, message = "INVALID_DOB")
+
     LocalDate dob;
     List<String> roles;
 }
