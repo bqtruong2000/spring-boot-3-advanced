d0f75f2b4e7138c7a29986d6196829aa51475994
bqtruong2000 <bqtruong2021@gmail.com>
2024-11-21 10:19:33 +0700

    #17-Unit test

---
+++ identity-service/src/test/java/core/identityservice/controller/UserControllerTest.java
@@ -0,0 +1,79 @@
+package core.identityservice.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import core.identityservice.dto.request.UserCreationRequest;
+import core.identityservice.dto.response.UserResponse;
+import core.identityservice.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+@Slf4j
+@AutoConfigureMockMvc
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    private UserCreationRequest request;
+    private UserResponse userResponse;
+    private LocalDate dob;
+
+    @BeforeEach
+    void initData() {
+        dob = LocalDate.of(1990, 1, 1);
+
+        request = UserCreationRequest.builder()
+                .username("john")
+                .firstName("John")
+                .lastName("Doe")
+                .password("123456")
+                .dob(dob)
+                .build();
+
+        userResponse = UserResponse.builder()
+                .id("cf0600f538b3")
+                .username("john")
+                .firstName("John")
+                .lastName("Doe")
+                .dob(dob)
+                .build();
+    }
+
+    @Test
+    public void createUser_validRequest_success() throws Exception {
+        // Given
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String content = objectMapper.writeValueAsString(request);
+
+        Mockito.when(userService.createRequest(ArgumentMatchers.any()))
+                .thenReturn(userResponse);
+
+        // When, Then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/users")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(content))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code")
+                        .value(1000)
+                );
+    }
+}
---
+++ identity-service/src/test/java/core/identityservice/service/UserServiceTest.java
@@ -0,0 +1,96 @@
+package core.identityservice.service;
+
+import core.identityservice.dto.request.UserCreationRequest;
+import core.identityservice.dto.response.UserResponse;
+import core.identityservice.entity.User;
+import core.identityservice.exception.AppException;
+import core.identityservice.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.util.Assert;
+
+import java.time.LocalDate;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Slf4j
+@AutoConfigureMockMvc
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    private UserCreationRequest request;
+    private UserResponse userResponse;
+    private User user;
+    private LocalDate dob;
+
+    @BeforeEach
+    void initData() {
+        dob = LocalDate.of(1990, 1, 1);
+
+        request = UserCreationRequest.builder()
+                .username("john")
+                .firstName("John")
+                .lastName("Doe")
+                .password("123456")
+                .dob(dob)
+                .build();
+
+        userResponse = UserResponse.builder()
+                .id("cf0600f538b3")
+                .username("john")
+                .firstName("John")
+                .lastName("Doe")
+                .dob(dob)
+                .build();
+
+        user = User.builder()
+                .id("cf0600f538b3")
+                .username("john")
+                .firstName("John")
+                .lastName("Doe")
+                .dob(dob)
+                .build();
+    }
+
+    @Test
+    public void createUser_validRequest_success() {
+        // Given
+        when(userRepository.existsByUsername(anyString())).thenReturn(false);
+        when(userRepository.save(any())).thenReturn(user);
+
+        // When
+        var response = userService.createRequest(request);
+
+        // Then
+        Assertions.assertThat(response.getId()).isEqualTo("cf0600f538b3");
+        Assertions.assertThat(response.getUsername()).isEqualTo("john");
+
+    }
+
+    @Test
+    public void createUser_userExisted_fail() {
+        // Given
+        when(userRepository.existsByUsername(anyString())).thenReturn(true);
+
+        // When
+        var exception = assertThrows(AppException.class,
+                () -> userService.createRequest(request));
+
+        Assertions.assertThat(exception.getMessage()).isEqualTo("User already exist");
+    }
+}
