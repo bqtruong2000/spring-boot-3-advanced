5fd2edbc695144807fb6b4f63669913ffa883988
bqtruong2000 <bqtruong2021@gmail.com>
2024-11-21 11:50:06 +0700

    #19-Integration test with Testcontainers

--- identity-service/src/test/java/core/identityservice/controller/UserControllerIntegrationTest.java
+++ identity-service/src/test/java/core/identityservice/controller/UserControllerIntegrationTest.java
@@ -4,34 +4,44 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import core.identityservice.dto.request.UserCreationRequest;
 import core.identityservice.dto.response.UserResponse;
-import core.identityservice.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDate;
 
 @SpringBootTest
 @Slf4j
 @AutoConfigureMockMvc
-@TestPropertySource("/test.properties")
-public class UserControllerTest {
+@Testcontainers
+public class UserControllerIntegrationTest {
+    @Container
+    static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER = new PostgreSQLContainer<>("postgres:13.2");
+
+    @DynamicPropertySource
+    static void configuraDataSource(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRE_SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRE_SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRE_SQL_CONTAINER::getPassword);
+        registry.add("spring.datasource.driverClassName", POSTGRE_SQL_CONTAINER::getDriverClassName);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+    }
+
+
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
-    private UserService userService;
 
     private UserCreationRequest request;
     private UserResponse userResponse;
@@ -45,16 +55,17 @@ public class UserControllerTest {
                 .username("johndoe")
                 .firstName("John")
                 .lastName("Doe")
-                .password("123456")
+                .password("12345678")
                 .dob(dob)
                 .build();
 
         userResponse = UserResponse.builder()
-                .id("cf0600f538b3")
+                .id("23e91564-324e-4177-93c2-e103c3502f05")
                 .username("johndoe")
                 .firstName("John")
                 .lastName("Doe")
                 .dob(dob)
+                .roles(null)
                 .build();
     }
 
@@ -65,17 +76,29 @@ public class UserControllerTest {
         objectMapper.registerModule(new JavaTimeModule());
         String content = objectMapper.writeValueAsString(request);
 
-        Mockito.when(userService.createRequest(ArgumentMatchers.any()))
-                .thenReturn(userResponse);
 
         // When, Then
-        mockMvc.perform(MockMvcRequestBuilders
+        var response = mockMvc.perform(MockMvcRequestBuilders
                         .post("/users/create")
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
                         .content(content))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("code")
                         .value(1000)
+
+                )
+                .andExpect(MockMvcResultMatchers.jsonPath("result.username")
+                        .value("johndoe")
+                )
+                .andExpect(MockMvcResultMatchers.jsonPath("result.firstName")
+                        .value("John")
+                )
+                .andExpect(MockMvcResultMatchers.jsonPath("result.lastName")
+                        .value("Doe")
+                )
+                .andExpect(MockMvcResultMatchers.jsonPath("result.dob")
+                        .value("1990-01-01")
                 );
+        log.info("Result: {}", response.andReturn().getResponse().getContentAsString());
     }
 }
--- identity-service/src/test/java/core/identityservice/service/UserServiceIntegrationTest.java
+++ identity-service/src/test/java/core/identityservice/service/UserServiceIntegrationTest.java
@@ -3,8 +3,6 @@ package core.identityservice.service;
 import core.identityservice.dto.request.UserCreationRequest;
 import core.identityservice.dto.response.UserResponse;
 import core.identityservice.entity.User;
-import core.identityservice.exception.AppException;
-import core.identityservice.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,27 +10,35 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @Slf4j
 @AutoConfigureMockMvc
-@TestPropertySource("/test.properties")
-public class UserServiceTest {
+@Testcontainers
+public class UserServiceIntegrationTest {
+
+
+    @Container
+    static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER = new PostgreSQLContainer<>("postgres:13.2");
+
+    @DynamicPropertySource
+    static void configuraDataSource(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRE_SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRE_SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRE_SQL_CONTAINER::getPassword);
+        registry.add("spring.datasource.driverClassName", POSTGRE_SQL_CONTAINER::getDriverClassName);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+    }
 
     @Autowired
     private UserService userService;
-
-    @MockBean
-    private UserRepository userRepository;
 
     private UserCreationRequest request;
     private UserResponse userResponse;
@@ -71,8 +77,8 @@ public class UserServiceTest {
     @Test
     public void createUser_validRequest_success() {
         // Given
-        when(userRepository.existsByUsername(anyString())).thenReturn(false);
-        when(userRepository.save(any())).thenReturn(user);
+//        when(userRepository.existsByUsername(anyString())).thenReturn(false);
+//        when(userRepository.save(any())).thenReturn(user);
 
         // When
         var response = userService.createRequest(request);
@@ -83,15 +89,15 @@ public class UserServiceTest {
 
     }
 
-    @Test
-    public void createUser_userExisted_fail() {
-        // Given
-        when(userRepository.existsByUsername(anyString())).thenReturn(true);
-
-        // When
-        var exception = assertThrows(AppException.class,
-                () -> userService.createRequest(request));
-
-        Assertions.assertThat(exception.getMessage()).isEqualTo("User already exist");
-    }
+//    @Test
+//    public void createUser_userExisted_fail() {
+//        // Given
+//        when(userRepository.existsByUsername(anyString())).thenReturn(true);
+//
+//        // When
+//        var exception = assertThrows(AppException.class,
+//                () -> userService.createRequest(request));
+//
+//        Assertions.assertThat(exception.getMessage()).isEqualTo("User already exist");
+//    }
 }
